d04b8d21ebbbe62cb68bc90f283a7d592f454aa7
Igor Chernyshev <sonic96.ic@gmail.com>
2023-01-05 01:47:00 +0500

    leave this tests for future

--- Tests/ApiClientTests/ClientsTests.cs
+++ Tests/ApiClientTests/ClientsTests.cs
@@ -13,13 +13,13 @@ public class ClientsTests
         loggerClient = LoggerClientBuilder.Build();
     }
 
-    [Test]
+    //[Test]
     public async Task TestApiTelemetryClient()
     {
         await apiTelemetryClient.CreateAsync("GET", "/", 200, 228);
     }
 
-    [Test]
+    //[Test]
     public async Task TestLoggerClient()
     {
         await loggerClient.InfoAsync("Test {a} {b} {c}", 1, 2, 3);

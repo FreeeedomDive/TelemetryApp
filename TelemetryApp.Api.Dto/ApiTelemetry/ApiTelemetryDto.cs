dff27fc4f8b3cdb8043c2f971aa9098966995b27
Igor Chernyshev <sonic96.ic@gmail.com>
2023-01-05 20:14:50 +0500

    remove unused Route field

--- TelemetryApp.Api.Dto/ApiTelemetry/ApiTelemetryDto.cs
+++ TelemetryApp.Api.Dto/ApiTelemetry/ApiTelemetryDto.cs
@@ -5,8 +5,7 @@ public class ApiTelemetryDto
     public string Project { get; set; }
     public string Service { get; set; }
     public string Method { get; set; }
-    public string? Route { get; set; }
-    public string? RoutePattern { get; set; }
+    public string RoutePattern { get; set; }
     public Dictionary<string, string> RouteParametersValues { get; set; }
     public int StatusCode { get; set; }
     public long ExecutionTime { get; set; }
